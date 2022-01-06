0aadf09c105794ce847765f68d61cf051701de14
daaeaad <daae0214@naver.com>
2022-01-06 17:31:01 +0900

    text데이터의 유효성에 확인 후 입력란 포커싱되도록 수정

--- mission/src/components/TextInputItem.vue
+++ mission/src/components/TextInputItem.vue
@@ -6,7 +6,9 @@
         placeholder="문자를 입력 해보세요."
         v-model="text"
         @input="handleInput"
+        :class="!isText ? 'focus' : ''"
     />
+    <span class="txt description_warn">위 입력란을 먼저 작성해주세요.</span>
 </div>
 <div class="blank_box_15"></div>
 </template>
@@ -19,13 +21,47 @@ export default {
       text: '',
     };
   },
+  props: {
+    isText: { type: Boolean },
+  },
+  emits: {
+    updateText: { type: Function },
+  },
   methods: {
     handleInput() {
       // console.log('handleInput ::::: ');
-
       const { text } = this;
       this.$emit('updateText', text);
     },
   },
 };
 </script>
+
+<style scoped>
+input.input_basic {
+  transition: 0.4s;
+}
+input.input_basic + .description_warn {display: none;
+  font-size: 16px;
+  padding: 10px;
+  color: #ff5f5f;
+}
+input.input_basic.focus {
+  border:1px solid #ff5f5f;
+  /* animation: focus_input 0.4s; */
+}
+input.input_basic.focus + .description_warn {display: block;}
+@keyframes focus_input {
+  0% {
+    transform: translateX(0px);
+  } 25% {
+    transform: translateX(-6px);
+  } 50% {
+    transform: translateX(0px);
+  } 75% {
+    transform: translateX(6px);
+  } 100% {
+    transform: translateX(0px);
+  }
+}
+</style>
