390e77f6377df13be3d788bcd6f1be5ea94ac6f4
daaeaad <daae0214@naver.com>
2022-01-12 16:01:47 +0900

    Fix: 실패 케이스 TC 삭제

--- mission/tests/unit/textView.spec.js
+++ mission/tests/unit/textView.spec.js
@@ -15,37 +15,29 @@ describe('TextView.vue - function', () => {
     home.vm.shakeText();
     expect(home.vm.text).toBe('231');
   });
-
-  test('실패 :: handleShakeText 함수를 실행하면 부모 컴포넌트로부터 받은 shakeText 함수에 실행을 요청하고, shakText는 text가 빈값이면 실행하지 않습니다.', () => {
-    const home = mount(Home);
-    home.vm.text = '';
-    wrapper.vm.handleShakeText();
-    home.vm.shakeText();
-    expect(home.vm.text).toBe('231');
-  });
 });
 
 describe('TextView.vue - trigger event', () => {
   let wrapper;
-  let spyFn = {};
+  const spyFn = {};
   beforeEach(() => {
-    spyFn.handleShakeText = jest.spyOn(TextView.methods, 'handleShakeText');
-    spyFn.handleClickBtn = jest.spyOn(TextView.methods, 'handleClickBtn');
     wrapper = mount(TextView, {
       props: {
         text: 'ABCD',
       },
     });
+    spyFn.handleShakeText = jest.spyOn(wrapper.vm.methods, 'handleShakeText');
+    spyFn.handleClickBtn = jest.spyOn(wrapper.vm.methods, 'handleClickBtn');
   });
 
-  test('성공 :: 글자 섞기 버튼을 클릭하면 handleShakeText 함수가 실행됩니다. ', () => {
-    const btn = wrapper.get('[data-test="btnShakeText"]');
+  test('성공 :: 글자 섞기 버튼을 클릭하면 handleShakeText 함수가 실행됩니다. ', async () => {
+    const btn = await wrapper.get('[data-test="btnShakeText"]');
     btn.trigger('click');
     expect(spyFn.handleShakeText).toHaveBeenCalled();
   });
 
-  test('성공 :: 알림 버튼을 클릭하면 handleClickBtn 함수가 실행됩니다.', () => {
-    const btn = wrapper.get('[data-test="handleClickBtn"]');
+  test('성공 :: 알림 버튼을 클릭하면 handleClickBtn 함수가 실행됩니다.', async () => {
+    const btn = await wrapper.get('[data-test="handleClickBtn"]');
     btn.trigger('click');
     expect(spyFn.handleClickBtn).toHaveBeenCalled();
   });
