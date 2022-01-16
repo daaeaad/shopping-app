9c5db9499d127d1fa72359e3a00334f16897423c
daaeaad <daae0214@naver.com>
2022-01-16 17:52:05 +0900

    feat: 상세페이지 상단 대표 사진 영역

---
+++ mission/src/components/ItemInfoVisual.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="itemInfoVisual" data-test="itemInfoVisual">
+    <div class="img_box square">
+      <div class="inner">
+        <div v-if="!data" class="img_frame flex row align_center">
+          <p class="lay_1 txt size_12 align_center padding_20 color_9b">
+            이미지를 불러올 수 없습니다.
+          </p>
+        </div>
+        <div v-if="data" class="img_frame">
+          <img :src="data.imgMain[0]" alt="" data-test="imgMain" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemInfoVisual',
+  data() {
+    return {
+      data: this.$store.getters.getProduct,
+    };
+  },
+  method: {},
+};
+</script>
+
+<style></style>
