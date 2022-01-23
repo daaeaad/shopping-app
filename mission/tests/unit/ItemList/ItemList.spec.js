d9c203eee675a12f67bf9b872c469d4f4ba74ee5
daaeaad <daae0214@naver.com>
2022-01-23 21:18:44 +0900

    test: 리스트 페이지 테스트 코드 수정

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -23,7 +23,7 @@ describe('ItemListPage', () => {
     expect(wrapper.findComponent({ name: 'ItemListItem' }).exists()).toBeTruthy();
   });
 
-  it('ItemListItem 컴포넌트는 datas 배열 개수만큼 렌더링 됩니다.', () => {
+  it('ItemListItem 컴포넌트는 products 배열 개수만큼 렌더링 됩니다.', () => {
     // const datas = [{ id: 'p1' }, { id: 'p2' }, { id: 'p3' }];
     // const mockFn = jest.fn().mockImplementation(() => {
     //   let result;
