cb872979c53a976f7c58351fae11619524c07a4d
daaeaad <daae0214@naver.com>
2022-02-04 04:38:43 +0900

    test: 상세페이지로 이동하는 router 테스트 코드 추가

--- mission/src/components/ItemList/ItemListItem.vue
+++ mission/src/components/ItemList/ItemListItem.vue
@@ -2,10 +2,14 @@
   <div class="item-list-item">
     <div class="img_box ratio ratio_10_7 position_relative">
       <div class="inner">
-        <a href="#" class="img_frame dis_block overflow_hidden">
+        <div
+          data-test="imageClick"
+          class="img_frame dis_block overflow_hidden"
+          @click="goItemDetail"
+        >
           <!-- 상품 이미지 -->
-          <img data-test="img" :src="imgMain" alt="" class="" />
-        </a>
+          <img data-test="image" :src="image" :alt="name" class="" />
+        </div>
       </div>
 
       <!-- 찜 버튼 { -->
@@ -13,8 +17,7 @@
         <!-- 버튼
          - 아이콘 스타일 활성화: .btn_like에 .act 클래스 추가 -->
 
-        <button type="button" class="btn_like position_relative">
-          <!-- :class="{ act: isLike }" -->
+        <button type="button" class="btn_like position_relative" :class="{ act: isLike }">
           <icon name="heart" class="ico heart_line" />
           <icon name="heart-fill" class="ico heart_fill" />
         </button>
@@ -23,12 +26,7 @@
     </div>
 
     <div class="txt_box align_left padding_10 padding_rl_15">
-      <a href="#" class="dis_block lay_1">
-        <!-- 판매자 상호명 -->
-        <p data-test="companyName" class="txt size_12 weight_m">
-          {{ companyName }}
-        </p>
-
+      <div data-test="textClick" class="dis_block lay_1" @click="goItemDetail">
         <div class="blank_box_10"></div>
 
         <!-- 상품명 -->
@@ -41,7 +39,7 @@
         <!-- 가격 -->
         <p data-test="price" class="txt size_14 weight_b">
           <!-- 할인율: 할인중일때만 표시 -->
-          <span data-test="discountPercent" v-if="isDiscount" class="color_red">
+          <span data-test="discountPercent" class="color_red" v-if="isDiscount">
             {{ discountPercent }}%
           </span>
           {{ currentPrice }}
@@ -49,93 +47,81 @@
 
         <div class="blank_box_10"></div>
 
-        <!-- 총 찜 횟수 -->
-        <p data-test="totalLike" class="txt size_10 weight_m color_98">
-          <icon name="heart-fill" class="ico" size="8" />
-          {{ getLocaleTotalLike }}
+        <!-- 상품 설명 -->
+        <p data-test="description" class="txt size_12 weight_m color_98 txt_overflow_1">
+          {{ description }}
         </p>
 
         <div class="blank_box_10"></div>
-      </a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { toRefs, ref, computed } from 'vue';
+import { toRefs } from 'vue';
+import { useRouter } from 'vue-router';
 import getDiscount from '@/composable/getDiscount';
 import getLocaleNumber from '@/composable/getLocaleNumber';
-
-/*
-const btnLike = (likeProduct, id) => {
-  const isLike = ref(false);
-
-  const getIsLike = () => {
-    const arr = Object.values(likeProduct.value);
-    let result = arr.filter((item) => item === id);
-
-    result = !!result.length;
-    return result;
-  };
-
-  isLike.value = getIsLike();
-
-  return { isLike };
-};
-*/
 
 export default {
   name: 'ItemListItem',
   props: {
-    product: {
-      type: Object,
-      required: true,
+    product_no: {
+      type: String,
+      default: '',
     },
-    likeProduct: {
-      type: Array,
-      required: true,
+    image: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    original_price: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    isLike: {
+      type: Boolean,
+      default: false,
     },
   },
 
   setup(props) {
-    // props
-    const {
-      product,
-      // likeProduct
-    } = toRefs(props);
-
-    // const { isLike } = btnLike(likeProduct, product.value.id);
-
-    // 상품 이미지
-    const imgMain = ref(product.value.imgMain);
-
-    // 상호명
-    const companyName = ref(product.value.store.companyName);
-
-    // 상품명
-    const name = ref(product.value.name);
-
-    // 총 찜 횟수
-    const getLocaleTotalLike = computed(() => product.value.totalLike.toLocaleString());
+    const { original_price: originalPrice, price, product_no: productNo } = toRefs(props);
 
     // 할인여부, 할인율
-    const { isDiscount, discountPercent } = getDiscount(
-      product.value.originalPrice,
-      product.value.currentPrice,
-    );
+    const { isDiscount, discountPercent } = getDiscount(originalPrice.value, price.value);
 
-    // 가격
-    const { result: currentPrice } = getLocaleNumber(product.value.currentPrice);
+    // 현재 가격
+    const { result: currentPrice } = getLocaleNumber(price.value);
+
+    // 상세페이지 링크
+    const router = useRouter();
+    const goItemDetail = () => {
+      router.push({
+        name: 'ItemDetail',
+        params: {
+          id: productNo.value,
+        },
+      });
+    };
 
     return {
-      imgMain,
-      companyName,
-      name,
       isDiscount,
       discountPercent,
       currentPrice,
-      getLocaleTotalLike,
-      // isLike,
+      goItemDetail,
     };
   },
 };
