cd2caf3dc913807d376a2572909800221c000cba
daaeaad <daae0214@naver.com>
2022-02-04 04:35:36 +0900

    test: api 호출 테스트 코드 추가

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,9 +1,21 @@
 import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemList.vue';
+import Repository from '@/repositories/RepositoryFactory';
+import Client from '@/repositories/Clients/AxiosClient';
+
+// axios mocking
+jest.mock('@/repositories/Clients/AxiosClient');
 
 describe('ItemListPage', () => {
   let wrapper;
   beforeEach(() => {
+    const items = [{ id: 123 }, { id: 456 }];
+    const res = {
+      data: {
+        items,
+      },
+    };
+    Client.get.mockImplementation(() => res);
     wrapper = mount(ItemInfoPage);
   });
 
@@ -24,13 +36,51 @@ describe('ItemListPage', () => {
   });
 
   it('ItemListItem 컴포넌트는 products 배열 개수만큼 렌더링 됩니다.', () => {
-    // const datas = [{ id: 'p1' }, { id: 'p2' }, { id: 'p3' }];
-    // const mockFn = jest.fn().mockImplementation(() => {
-    //   let result;
-    //   return result;
-    // });
-    // const test = mockFn();
-
     expect(wrapper.findAllComponents({ name: 'ItemListItem' }).length).toBe(2);
   });
 });
+
+/* Repository Test { */
+
+describe('api 호출 테스트', () => {
+  it('상품 목록 data', async () => {
+    // setup
+    const items = [{ id: 123 }];
+    const res = {
+      data: {
+        items,
+      },
+    };
+    Client.get.mockImplementation(() => res);
+
+    // act
+    const ItemRepository = Repository.get('item');
+    const getList = await ItemRepository.getList();
+
+    // result
+    expect(Client.get).toHaveBeenCalled();
+    expect(Client.get).toHaveBeenCalledWith('/item');
+    expect(getList).toEqual(items);
+  });
+
+  it('찜 목록 data', async () => {
+    // setup
+    const items = [{ id: 123 }, { id: 311 }];
+    const res = {
+      data: {
+        items,
+      },
+    };
+    Client.get.mockImplementation(() => res);
+
+    // act
+    const WishRepository = Repository.get('wish');
+    const getList = await WishRepository.getList();
+
+    // result
+    expect(Client.get).toHaveBeenCalled();
+    expect(Client.get).toHaveBeenCalledWith('/wish');
+    expect(getList).toEqual(items);
+  });
+});
+/* } Repository Test 끝 */
