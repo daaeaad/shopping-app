70320ffd44563e948b389351a5e283d8ee2ac191
daaeaad <daae0214@naver.com>
2022-02-13 09:15:43 +0900

    feat: 주문하기 페이지 및 기능 추가

---
+++ mission/src/components/InputGroup/RadioButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="ck_box rdo_btn">
+    <label class="size_14 weight_m">
+      <input type="radio" v-model="inputValue" id="" :value="detailValue" />
+      <div class="box">{{ content }}</div>
+    </label>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'RadioButton',
+  components: {},
+  props: {
+    content: {
+      type: String,
+      default: '',
+    },
+    detailValue: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props, { emit }) {
+    const inputValue = computed({
+      get: () => props.modelValue,
+      set: (value) => emit('update:modelValue', value),
+    });
+
+    return {
+      inputValue,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ mission/src/components/InputGroup/TitleItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="line hori bold"></div>
+  <div class="title_sec align_left">
+    <p class="txt size_16 weight_eb">
+      {{ title }}
+    </p>
+    <p v-if="subTitle" class="txt size_14 color_98">
+      {{ subTitle }}
+    </p>
+  </div>
+  <div class="line hori"></div>
+</template>
+
+<script>
+export default {
+  name: 'TitleItem',
+  components: {},
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    subTitle: {
+      type: String,
+      default: '',
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
