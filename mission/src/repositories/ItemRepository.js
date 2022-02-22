12f393a5a9354582d74166e73f4d4c62356edbd6
daaeaad <daae0214@naver.com>
2022-02-22 22:48:06 +0900

    feat: 장바구니, 주문상품정보 관련 api 호출 추가

--- mission/src/repositories/ItemRepository.js
+++ mission/src/repositories/ItemRepository.js
@@ -1,4 +1,3 @@
-// import Client from '@/repositories/Clients/AxiosClient';
 import { getData } from '@/repositories/DataHandler';
 
 const resource = '/item';
@@ -9,10 +8,46 @@ export default {
   // 목록
   getList() {
     return getData(`${resource}`, depth);
-    // return Client.get(`${resource}`);
   },
   // 상세
   getDetail(id) {
-    return getData(`${resource}/${id}`, 'item');
+    return getData(`item_detail/${id}`, 'item');
+  },
+
+  //
+  getOrderList(id) {
+    return getData('/search_list', '', { id });
   },
 };
+
+/*
+class OrderItemModel {
+  constructor(
+    id,
+    name,
+    image,
+    originalPrice,
+    price,
+    stockCount,
+    description,
+    like,
+    options,
+    store,
+    quantity,
+    totalPrice,
+  ) {
+    this.product_no = id;
+    this.name = name;
+    this.image = image;
+    this.original_price = originalPrice;
+    this.price = price;
+    this.stock_count = !stockCount ? 0 : stockCount;
+    this.description = description;
+    this.like = like;
+    this.options = options;
+    this.store = store;
+    this.quantity = quantity;
+    this.totalPrice = totalPrice;
+  }
+}
+*/
