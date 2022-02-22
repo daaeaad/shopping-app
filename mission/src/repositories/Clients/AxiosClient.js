38adce43fa4e8bdc9605f685ade814f2022ded00
daaeaad <daae0214@naver.com>
2022-02-22 22:45:29 +0900

    feat: mock api 사용을 위한 변경사항 반영

--- mission/src/repositories/Clients/AxiosClient.js
+++ mission/src/repositories/Clients/AxiosClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseDomain = 'https://virtserver.swaggerhub.com/lkaybob/projectlion-vue/1.0.0';
+// const baseDomain = 'https://virtserver.swaggerhub.com/lkaybob/projectlion-vue/1.0.0';
+const baseDomain = 'http://localhost:8081';
 const baseURL = `${baseDomain}`;
 
 const Authorization = 'abcd1234';
